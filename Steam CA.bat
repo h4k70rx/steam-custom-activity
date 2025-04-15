6ace4bf80ab96d9264ae0cf4cacd15ccd4929149
Mr.X <55602145+kpuc313@users.noreply.github.com>
2025-04-15 14:52:02 +0300

    Update Steam CA.bat

--- Steam CA.bat
+++ Steam CA.bat
@@ -11,12 +11,7 @@ rem [ APPLICATION ]=======================================================
 title %AppName%
 
 if not exist "Steam.exe" (
-	(
-		echo [ERROR]: Cannot find Steam.exe!
-		echo [ERROR]: Please, put the program in the Steam.exe directory!
-		echo.
-		echo *** YOU CAN DELETE THIS FILE! ***
-	) > "%~f0 ERROR.txt"
+	call :fn_popup "%AppName%" "Please, put the program in the Steam.exe directory!" "Warning"
 	exit
 )
 
@@ -36,4 +31,22 @@ if not exist "%~f0.%AppExtName%" (
 	taskkill /F /FI "WindowTitle eq  %AppAdmin%  %AppName%" /T
 	exit
 )
+rem ======================================================================
+
+rem [ FUNCTIONS ]==========================================================
+:: *** Function PopUP v1.0 - Show notification popup ***
+:: Usage: call :fn_popup "Title" "Message" "Icon" "Time to show in seconds (Default: 0-Unlimited[OK button needs to be pressed])"
+:: Icon Types: None, Error, Question, Warning, Info
+:fn_popup
+if "%~1" == "" (set PopupTitle="Default Title") else (set PopupTitle=%1)
+if "%~2" == "" (set PopupMessage="Default Message") else (set PopupMessage=%2)
+if "%~3" == "" (set PopupIcon=0)
+if "%~3" == "None" (set PopupIcon=0)
+if "%~3" == "Error" (set PopupIcon=10)
+if "%~3" == "Question" (set PopupIcon=20)
+if "%~3" == "Warning" (set PopupIcon=30)
+if "%~3" == "Info" (set PopupIcon=40)
+if "%~4" == "" (set PopupTime=0) else (set PopupTime=%4)
+powershell -Command "(New-Object -ComObject Wscript.Shell).Popup('%PopupMessage%', %PopupTime%, '%PopupTitle%', 0x%PopupIcon%)"
+exit /B
 rem ======================================================================
